19c70ae6ee53361bc021600abea5d4c8b9b60bb1
mig9tx <migue.trujillo@utexas.edu>
2018-08-28 22:44:21 -0500

    made additional coding modifications as score was not being kept correctly. now losses goes up by one after 10 failed guesses and guess counter is reset.  Wins and win guess reset are working properly. now I just need to fix the string to show up properly when guessing letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,53 @@
-     //create an array of letters
-     let compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-     //console.log(compChoice[5]);
- var wins = 0;
- var losses = 0;
- var guessLeft = 10;
- var guessTotal = [];
+//create an array of letters
+let compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//console.log(compChoice[5]);
+var wins = 0;
+var losses = 0;
+var guessLeft = 10;
+var guessTotal = "";
 
- //create variables to hold the values for the HTML elements to be displayed
- var instructionsText = document.getElementById("instructions-text");
- var winsText = document.getElementById("wins-text");
- var lossesText = document.getElementById("losses-text");
- var guessesLeft = document.getElementById("guessleft-text");
- var guessText = document.getElementById("guess-text");  
+//create variables to hold the values for the HTML elements to be displayed
+var instructionsText = document.getElementById("instructions-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeft = document.getElementById("guessleft-text");
+var guessText = document.getElementById("guess-text");
 
 
- //user inputs key as his first guess, save into a variable
- 
- document.onkeyup = function(event) {
-     var userGuess = event.key;
-     // var event = guessTotal.push(event)
-     console.log(event);
-     //computer selects a letter from the array randomly based on the length of the array.
-     var compLetter = compChoice[Math.floor(Math.random() * compChoice.length)];
- 
- //save input and compare to compChoice
- if (userGuess === compLetter){
-     wins++;
- } else if (userGuess !== compLetter){
-     guessLeft--;
-     losses++;
- }
- if (losses >= 10)
- alert("Game Over     Reload Page to Play Again")
- //hides the instructions
- // instructionsText.textContent = "";
+//user inputs key as his first guess, save into a variable
 
- //displays variable values to the browser window along with text
- document.getElementById("guess-text").innerHTML = "You chose: " + userGuess;
- document.getElementById("guessleft-text").innerHTML = "Guesses left: " + guessLeft;
- document.getElementById("wins-text").innerHTML = "Wins: " + wins;
- document.getElementById("losses-text").innerHTML = "Losses: " + losses;
-//  guessText.textContent("guess-text") = "You chose: " + userGuess;
-//  guessesLeft.textContent = "Guesses left: " + guessLeft;
-//  winsText.textContent = "Wins: " + wins;
-//  lossesText.textContent = "Losses: " + losses;
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    guessTotal = guessTotal + userGuess;
+    // var event = guessTotal.push(event)
+    console.log(event);
+    //computer selects a letter from the array randomly based on the length of the array.
+    var compLetter = compChoice[Math.floor(Math.random() * compChoice.length)];
+
+    //save input and compare to compChoice
+    if (userGuess === compLetter){
+        wins++;
+        guessLeft=10;
+    }else if(userGuess !== compLetter){
+        guessLeft--;
+        // losses++;
+    }if (guessLeft <= 0){
+        losses++;
+        guessLeft = 10;
 }
+
+
+
+//hides the instructions
+// instructionsText.textContent = "";
+
+//displays variable values to the browser window along with text
+document.getElementById("guess-text").innerHTML = "You chose: " + userGuess;
+document.getElementById("guessleft-text").innerHTML = "Guesses left: " + guessLeft;
+document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+document.getElementById("losses-text").innerHTML = "Losses: " + losses;
+
+}
+
+
 
